2209d2b445ba55fd13198b1b902bca7e726e903e
harasahiti <109272550+harasahiti@users.noreply.github.com>
2023-07-06 18:41:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function convertToCelsius() {
+    let celsiusInput = document.getElementById("celsius");
+    let fahrenheitInput = document.getElementById("fahrenheit");
+    
+    if (celsiusInput.value !== "") {
+      let celsius = parseFloat(celsiusInput.value);
+      let fahrenheit = (celsius * 9/5) + 32;
+      fahrenheitInput.value = fahrenheit.toFixed(2);
+    } else {
+      fahrenheitInput.value = "";
+    }
+  }
+  
+  function convertToFahrenheit() {
+    let celsiusInput = document.getElementById("celsius");
+    let fahrenheitInput = document.getElementById("fahrenheit");
+    
+    if (fahrenheitInput.value !== "") {
+      let fahrenheit = parseFloat(fahrenheitInput.value);
+      let celsius = (fahrenheit - 32) * 5/9;
+      celsiusInput.value = celsius.toFixed(2);
+    } else {
+      celsiusInput.value = "";
+    }
+  }
+  
